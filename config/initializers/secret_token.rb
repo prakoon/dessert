076330f69dd09078114327a4c70979950711aee4
Prakoon <prakoon@fabkids.com>
2013-04-02 16:13:14 -0700

    First checkin for Dessert

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dessert::Application.config.secret_token = 'ab861165c4b2ddf6783d7627dd03f9eb4cbf902c6a37abb8ca30b7a318787d2be7eac0e9329e4a5f485bee6825b3a2bc79793b215fc4862fe689ba31330d8c36'
