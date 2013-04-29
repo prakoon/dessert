5efd8a0bc7ad95e4b45fe27fb35e9f039bf0ffbf
Prakoon <prakoon@fabkids.com>
2013-04-29 15:19:12 -0700

    - Change config to use Rails.application.config instead of Dessert::Application.config - Change the session token to be _ds_session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def index
-    @test_message = Dessert::Application.config.test_home_message
+    @test_message = Rails.application.config.test_home_message
   end
 end
